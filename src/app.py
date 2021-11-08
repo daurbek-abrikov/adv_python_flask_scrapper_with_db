4c1089ea8072f33dd161032aaf9d5aa2ab23d9c2
Lelxuch <nakyp.sanzhar@gmail.com>
2021-11-08 15:21:12 +0600

    Update app.py
    Coin paragraphs replaced with news

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/python_flask_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:PASSWORD@localhost:5432/NAME'
 app.config['SECRET_KEY'] = 'thisismyflasksecretkey'
 
 db = SQLAlchemy(app)
+
+
+def refactoreCoinName(coin): # For example "Binance Coin" -> "binance-coin"
+    result = str(coin.lower())
+    splittedCoin = result.split()
+    if len(splittedCoin) > 1:
+      result = str(splittedCoin[0])
+      for i in range(1, len(splittedCoin)):
+        result += "-" + splittedCoin[i]
+    return result
 
 
 @app.route('/')
@@ -18,20 +29,26 @@ def home():
 
 @app.route('/coin', methods=['POST'])
 def find_coin():
-    coin_name = request.form.get('coin_name')
+    coin_name = refactoreCoinName(request.form.get('coin_name'))
 
     res = News.query.filter_by(coin=coin_name).all()
 
     if not res:
-        URL = "https://coinmarketcap.com/currencies/" + coin_name +"/"
-        r = requests.get(URL)
+        URL = "https://coinmarketcap.com/currencies/" + coin_name +"/news/"
+        opts = webdriver.ChromeOptions()
+        opts.headless =True
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-        header_tags = soup.find_all(['h1'])
-        paragraph_tags = soup.find_all(['p'])
+        r = browser.get(URL)
+
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        headers_list = soup.find_all('h3', {'class': 'sc-1q9q90x-0 gEZmSc'})
+        p_list = soup.find_all('p', {'class': 'q7nmo0-0 svowul-3 ddtKCV'})
+        headers_text = [h.text for h in headers_list]
+        p_text = [p.text for p in p_list]
         
-        for p in paragraph_tags:
-            news = News(coin=coin_name, header=header_tags[0].text, paragraph=p.text)
+        for i in range(0, len(headers_text)):
+            news = News(coin=coin_name, header=headers_text[i], paragraph=p_text[i])
             db.session.add(news)
         db.session.commit()
 
@@ -40,7 +57,6 @@ def find_coin():
     
 
     return render_template('coin.html', results=res)
-    
 
 @app.route('/coin', methods=['GET'])
 def coin():
@@ -64,17 +80,9 @@ class News(db.Model):
         return f"News('{self.header}', '{self.paragraph}')"
 
 
-
 db.drop_all()
 db.create_all()
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
